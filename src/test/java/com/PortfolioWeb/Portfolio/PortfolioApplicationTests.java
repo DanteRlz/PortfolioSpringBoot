8a13a2959dc24efe5cb5a59496ab92da61afd96b
Dante <dante_martinelli@hotmail.com>
2022-07-31 00:56:12 -0300

    Enpezando heroku

---
+++ src/test/java/com/PortfolioWeb/Portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PortfolioWeb.Portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
